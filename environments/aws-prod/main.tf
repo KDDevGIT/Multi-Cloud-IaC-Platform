dc788bbbba4f7f5d56e8cd99b4fcad3acb52e825
Kyler Dana <kdana@axon.com>
2026-02-27 09:09:24 -0700

    Added environments > aws-prod > main.tf for Minimal Prod Stack Env. Root Modules

---
+++ environments/aws-prod/main.tf
@@ -0,0 +1,26 @@
+provider "aws" {
+    region = var.aws_region
+}
+
+module "network" {
+    source = "../../module/aws/network"
+    name = var.name
+    cidr_block = var.vpc_cidr
+}
+
+module "compute" {
+  source = "../../module/aws/compute"
+  name = var.name
+  vpc_id = module.network.vpc_id
+  public_subnets = module.network.public_subnet_ids
+  private_subnets = module.network.private_subnet_ids
+}
+
+module "database" {
+    source = "../../modules/aws/database"
+    name = var.name
+    vpc_id = module.network.vpc_id
+    private_subnets = module.network.private_subnet_ids
+    db_username = var.db_username
+    db_password = var.db_password
+}
