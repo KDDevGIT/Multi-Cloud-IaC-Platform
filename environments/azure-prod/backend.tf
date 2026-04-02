72210c59ff379e4308bc9ef1973d81c8ce4d4171
KDDevGIT <kymasterbox@gmail.com>
2026-04-02 13:40:14 -0700

    updated backend.tf for AWS and Azure

--- environments/azure-prod/backend.tf
+++ environments/azure-prod/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "azurerm" {
-    resource_group_name = "TFSTATE_NAME_RG" 
-    storage_account_name = "TF_STATE_STORAGE" 
-    container_name = "tfstate"
+    resource_group_name = "platform-rg" 
+    storage_account_name = "platformstoragekdtest" 
+    container_name = "tfstatecontainerkd"
     key = "azure-prod.terraform.tfstate"
   }
 }
