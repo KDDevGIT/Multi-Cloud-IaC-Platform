d658d63913a64d73826e25322b516c1763e187df
KDDevGIT <kymasterbox@gmail.com>
2026-04-03 09:08:28 -0700

    Updated modules > aws > network > outputs.tf to make outputs accessible

--- modules/aws/network/outputs.tf
+++ modules/aws/network/outputs.tf
@@ -1,11 +1,14 @@
 output "vpc_id" {
+  description = "ID of the VPC"
   value = aws_vpc.this.id
 }
 
-/*output "public_subnet_ids" {
+output "public_subnet_ids" {
+  description = "ID of Public Subnets"
   value = [for s in aws_subnet.public : s.id]
 }
 
 output "private_subnet_ids" {
+  description = "ID of Private Subnets"
   value = [for s in aws_subnet.private : s.id]
-}*/
+}
