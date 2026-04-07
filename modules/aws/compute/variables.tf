3404e9aca306d65b3976355ce072509cc90f959d
KDDevGIT <kymasterbox@gmail.com>
2026-04-07 12:47:55 -0700

    Updated modules/aws/compute/variables.tf for ALB Implementation

--- modules/aws/compute/variables.tf
+++ modules/aws/compute/variables.tf
@@ -23,3 +23,22 @@ variable "ami_id" {
   type = string
   description = "AMI ID for the EC2 Instance"
 }
+
+//Application Load Balancer Implmentation
+variable "desired_capacity" {
+  type = number
+  description = "Desired No. of EC2 Instances"
+  default = 2
+}
+
+variable "min_size" {
+  type = number
+  description = "Minimum No. of EC2 Instances"
+  default = 2
+}
+
+variable "max_size" {
+  type = number
+  description = "Maximum No. of EC2 Instances"
+  default = 3
+}
