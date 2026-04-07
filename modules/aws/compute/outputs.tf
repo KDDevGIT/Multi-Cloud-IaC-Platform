7b88bbecc1a271b73e9c841f28f3c08ce094eb79
KDDevGIT <kymasterbox@gmail.com>
2026-04-07 13:12:32 -0700

    Updated modules/aws/compute/outputs.tf for ALB Implementation

--- modules/aws/compute/outputs.tf
+++ modules/aws/compute/outputs.tf
@@ -1,4 +1,31 @@
-output "instance_id" {
+//New Config (With ALB)
+output "alb_dns_name" {
+  description = "DNS name of the Application Load Balancer."
+  value       = aws_lb.this.dns_name
+}
+
+output "target_group_arn" {
+  description = "ARN of the ALB target group."
+  value       = aws_lb_target_group.this.arn
+}
+
+output "alb_security_group_id" {
+  description = "Security group ID attached to the ALB."
+  value       = aws_security_group.alb.id
+}
+
+output "ec2_security_group_id" {
+  description = "Security group ID attached to EC2 instances."
+  value       = aws_security_group.ec2.id
+}
+
+output "autoscaling_group_name" {
+  description = "Name of the Auto Scaling Group."
+  value       = aws_autoscaling_group.this.name
+}
+
+//Old Config (No ALB)
+/*output "instance_id" {
   description = "ID of the EC2 Instance"
   value = aws_instance.this.id 
 }
@@ -11,4 +38,4 @@ output "public_ip" {
 output "security_group_id" {
   description = "Security Group ID attached to EC2 Instance"
   value = aws_security_group.this.id 
-}
+}*/
