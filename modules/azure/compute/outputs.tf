252460a2ea849dea57ab7e65b041bbdad4a9e720
KDDevGIT <kymasterbox@gmail.com>
2026-04-08 14:11:36 -0700

    Updated modules/azure/compute/outputs.tf for Load Balancer + VM Scale Set Implementation

--- modules/azure/compute/outputs.tf
+++ modules/azure/compute/outputs.tf
@@ -1,14 +1,19 @@
-output "vm_id" {
-  description = "ID of the Azure Linux VM."
-  value       = azurerm_linux_virtual_machine.this.id
+output "load_balancer_id" {
+  description = "Azure Load Balancer ID."
+  value       = azurerm_lb.this.id
 }
 
-output "public_ip_address" {
-  description = "Public IP address assigned to the VM."
-  value       = azurerm_public_ip.this.ip_address
+output "load_balancer_public_ip" {
+  description = "Public IP address of the Azure Load Balancer."
+  value       = azurerm_public_ip.lb.ip_address
 }
 
-output "network_interface_id" {
-  description = "NIC ID attached to the VM."
-  value       = azurerm_network_interface.this.id
+output "vm_scale_set_id" {
+  description = "Azure VM Scale Set ID."
+  value       = azurerm_linux_virtual_machine_scale_set.this.id
+}
+
+output "network_security_group_id" {
+  description = "Azure NSG ID for the VM Scale Set."
+  value       = azurerm_network_security_group.this.id
 }
